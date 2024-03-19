32b71964cf3b938c736fa6361d97e5052e1f7123
Carlos Andres <carams500@hotmail.com>
2024-03-19 09:02:47 +0100

    JunitTestRunner to emulate Hilt

--- app/src/androidTest/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/notes/NotesScreenKtTest.kt
+++ app/src/androidTest/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/notes/NotesScreenKtTest.kt
@@ -1,12 +1,51 @@
 package com.plcoding.cleanarchitecturenoteapp.feature_note.presentation.notes
 
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.plcoding.cleanarchitecturenoteapp.di.AppModule
+import com.plcoding.cleanarchitecturenoteapp.feature_note.presentation.MainActivity
+import com.plcoding.cleanarchitecturenoteapp.feature_note.presentation.util.Screen
+import com.plcoding.cleanarchitecturenoteapp.ui.theme.CleanArchitectureNoteAppTheme
+import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import dagger.hilt.android.testing.UninstallModules
+import org.junit.After
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
 
 @HiltAndroidTest
 @UninstallModules(AppModule::class)
 class NotesScreenKtTest {
 
+    @get:Rule(order = 0)
+    val hiltRule = HiltAndroidRule(this)
+
+    @get:Rule(order = 1)
+    val composeRule = createAndroidComposeRule<MainActivity>()
+
+    @OptIn(ExperimentalAnimationApi::class)
+    @Before
+    fun setUp() {
+        hiltRule.inject()
+        composeRule.setContent {
+            val navController = rememberNavController()
+            CleanArchitectureNoteAppTheme {
+                NavHost(navController = navController, startDestination = Screen.NotesScreen.route) {
+                    composable(route = Screen.NotesScreen.route) {
+                        NotesScreen(navController = navController)
+                    }
+                }
+            }
+        }
+    }
+
+    @After
+    fun tearDown() {
+
+    }
 }
